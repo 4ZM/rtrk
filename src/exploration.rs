b883c56e60d55b231c09f67299b04bdce8f4becc
4ZM <anders@4zm.org>
2025-02-15 19:29:17 +0100

    Callbacks not really working

--- src/exploration.rs
+++ src/exploration.rs
@@ -1,14 +1,33 @@
 use std::io;
 use std::io::Cursor;
 
-struct DataModel {
+struct DataModel<'a> {
+    listeners: Vec<Box<dyn FnMut() + 'a>>,
     num: u8,
+}
+
+impl<'a> DataModel<'a> {
+    fn add_listener(&mut self, l: impl FnMut() + 'a) {
+        self.listeners.push(Box::new(l));
+    }
+
+    fn set_num(&mut self, new_num: u8) {
+        self.num = new_num;
+        for l in self.listeners.iter_mut() {
+            l();
+        }
+    }
 }
 
 struct ViewModel {
     repeat: u8,
 }
 impl ViewModel {
+    fn dm_changed(&mut self) {
+        println!("IN VM CB");
+        self.repeat += 1;
+    }
+
     fn num(&self, dm: &DataModel) -> String {
         let mut n: String = String::new();
 
@@ -36,10 +55,16 @@ mod tests {
     fn exploration_test() {
         let mut buffer = Cursor::new(Vec::new());
 
-        let dm: DataModel = DataModel { num: 23 };
-        let vm: ViewModel = ViewModel { repeat: 2 };
         let view: View = View {};
+        let mut vm: ViewModel = ViewModel { repeat: 2 };
+        let mut dm: DataModel = DataModel {
+            num: 23,
+            listeners: vec![],
+        };
+
         view.render(&mut buffer, &vm, &dm);
+        dm.add_listener(|| ViewModel::dm_changed(&mut vm));
+        dm.set_num(23);
 
         let output = String::from_utf8(buffer.into_inner()).unwrap();
         assert_eq!(output, "[2323]");
